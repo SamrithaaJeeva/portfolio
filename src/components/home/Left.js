f199617852fe484531c1d267e728f5dd4e31ab38
Kavinesh G <indranigk@gmail.com>
2024-06-12 20:16:48 +0530

    Commit-Final

--- src/components/home/Left.js
+++ src/components/home/Left.js
@@ -5,7 +5,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { AiFillTwitterCircle } from "react-icons/ai";
 
 import { FiMail } from "react-icons/fi";
-import CV from "../../assets/Curriculum Vitae - Samrithaa.pdf.pdf";
+import CV from "../../assets/Resume.pdf";
 import { bannerImg } from "../../assets/index";
 
 const Left = () => {
